42da22b7aae065954c9678c8738c1cb918672c04
zouhecan <hc.zou@ctrip.com>
2022-09-14 13:18:03 +0800

    添加注释

--- app/src/main/java/com/example/aspect/JumpAspect.java
+++ app/src/main/java/com/example/aspect/JumpAspect.java
@@ -9,6 +9,7 @@ import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class JumpAspect {
+    //指定切点的位置在com.example.MainActivity.jump()方法
     @Pointcut("execution(* com.example.MainActivity.jump(..))")
     public void pointcut() {
 
@@ -17,8 +18,13 @@ public class JumpAspect {
     @Around("pointcut()")
     public void aroundJump(ProceedingJoinPoint point) {
         try {
+            //在com.example.MainActivity.jump()执行前
             Log.d("JumpAspect", "准备跳页");
+
+            //com.example.MainActivity.jump()执行
             point.proceed();
+
+            //在com.example.MainActivity.jump()执行后
             Log.d("JumpAspect", "完成跳页");
         } catch (Throwable e) {
             e.printStackTrace();
--- lib/src/main/java/com/example/lib/annotation/TestModel.java
+++ lib/src/main/java/com/example/lib/annotation/TestModel.java
@@ -4,21 +4,11 @@ public class TestModel {
     @MyField(description = "姓名")
     private String name;
 
-    private int length;
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
     }
 }
